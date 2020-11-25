92afd587ccbba28cd38d45c3544c7309e9794d0c
gulgulia17 <gulgulia17@gmail.com>
2020-11-26 00:04:56 +0530

    Meta Title Fixed v0.5

--- resources/views/auth/app.blade.php
+++ resources/views/auth/app.blade.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>@yield('title','Venus')</title>
+    <title>@yield('title',config('app.name'))</title>
     <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css" />
     <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/icheck-bootstrap/3.0.1/icheck-bootstrap.min.css">
     <link rel="stylesheet"
